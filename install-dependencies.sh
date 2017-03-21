2172c4192e4bf20b2e124655fbcaa85809abe22c
Dani Hodovic <dani.hodovic@gmail.com>
2017-03-21 10:56:39 +0100

    Cache go get dependencies
    Running 'go get' after copying the source files causes us to re-build
    all dependencies on every source file change.
    If we instead specify our dependencies in install-dependencies.sh we can
    install and cache them before installing the source files. It's a ghetto
    approach, but greatly improves build times between tests.

---
+++ install-dependencies.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Let's avoid rebuilding the dependencies each time a source file changes.
+# See: http://stackoverflow.com/questions/39278756/cache-go-get-in-docker-build
+go get github.com/hashicorp/terraform/terraform
+go get github.com/hashicorp/terraform/helper/resource
+go get github.com/hashicorp/terraform/helper/schema
+go get github.com/tolgaakyuz/contentful-go
