b222fe4cb76589d585a9b575b4706adaf3e4063e
Nathan Barraille <nathan.barraille@gmail.com>
2018-08-27 12:41:23 +0200

    Fixed contentful-go repo address

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,4 +4,4 @@
 go get github.com/hashicorp/terraform/terraform
 go get github.com/hashicorp/terraform/helper/resource
 go get github.com/hashicorp/terraform/helper/schema
-go get github.com/tolgaakyuz/contentful-go
+go get github.com/contentful-labs/contentful-go
